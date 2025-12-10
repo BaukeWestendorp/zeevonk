b1c88c2c1bfadec7bbd1e786cd4fd18d686ca1bd
Bauke Westendorp <baukewestendorp@gmail.com>
2025-12-10 13:22:18 +0100

    Remove IntervalDmxOutput packet variant

--- src/packet/client.rs
+++ src/packet/client.rs
@@ -4,10 +4,6 @@ use crate::packet::PacketPayload;
 /// Packets sent from the server to the client.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum ClientboundPacketPayload {
-    /// Response containing DMX output data for all universes.
-    /// Sent at regular intervals.
-    IntervalDmxOutput,
-
     /// Response containing the layout information.
     ResponseLayout,
 
@@ -27,21 +23,18 @@ pub enum ClientboundPacketPayload {
 impl PacketPayload for ClientboundPacketPayload {
     fn id(&self) -> u8 {
         match self {
-            Self::IntervalDmxOutput => 0,
-
-            Self::ResponseLayout => 1,
-            Self::ResponseDmxOutput(_) => 2,
-            Self::ResponseTriggers => 3,
-            Self::ResponseAttributeValues => 4,
-            Self::ResponseSetAttributeValues => 5,
+            Self::ResponseLayout => 0,
+            Self::ResponseDmxOutput(_) => 1,
+            Self::ResponseTriggers => 2,
+            Self::ResponseAttributeValues => 3,
+            Self::ResponseSetAttributeValues => 4,
         }
     }
 
     fn from_id_and_data(id: u8, data: &[u8]) -> Result<Self, super::Error> {
         match id {
-            0 => Ok(Self::IntervalDmxOutput),
-            1 => Ok(Self::ResponseLayout),
-            2 => {
+            0 => Ok(Self::ResponseLayout),
+            1 => {
                 let multiverse =
                     rmp_serde::from_slice(data).map_err(|_| super::Error::InvalidPayload {
                         message: "failed to deserialize Multiverse".to_string(),
@@ -49,17 +42,16 @@ impl PacketPayload for ClientboundPacketPayload {
 
                 Ok(Self::ResponseDmxOutput(multiverse))
             }
-            3 => Ok(Self::ResponseTriggers),
-            4 => Ok(Self::ResponseAttributeValues),
-            5 => Ok(Self::ResponseSetAttributeValues),
+            2 => Ok(Self::ResponseTriggers),
+            3 => Ok(Self::ResponseAttributeValues),
+            4 => Ok(Self::ResponseSetAttributeValues),
             _ => Err(super::Error::InvalidPacketId(id)),
         }
     }
 
     fn to_data_bytes(&self) -> Result<Vec<u8>, super::Error> {
         match self {
-            Self::IntervalDmxOutput
-            | Self::ResponseLayout
+            Self::ResponseLayout
             | Self::ResponseTriggers
             | Self::ResponseAttributeValues
             | Self::ResponseSetAttributeValues => Ok(Vec::new()),
