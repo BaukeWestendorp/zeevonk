7090cdcce313986ef9be3d767105c65cce9898b9
Bauke Westendorp <baukewestendorp@gmail.com>
2026-01-13 17:02:10 +0100

    Remove sACN receiver

--- crates/zeevonk/src/server/protocols/sacn/mod.rs
+++ crates/zeevonk/src/server/protocols/sacn/mod.rs
@@ -8,13 +8,10 @@
 
 pub(crate) mod acn;
 pub mod packet;
-pub mod receiver;
 pub mod source;
 
 use arrayvec::ArrayVec;
 
-#[allow(unused_imports)]
-pub use receiver::*;
 pub use source::*;
 
 /// # E1.31 3.2 Universe
