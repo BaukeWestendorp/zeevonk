ed2e548c273200ff8f9ed7caf97bcf137288c1a9
Bauke Westendorp <baukewestendorp@gmail.com>
2026-01-20 21:34:02 +0100

    Add helpers for 1Identifier1

--- crates/zeevonk/src/ident.rs
+++ crates/zeevonk/src/ident.rs
@@ -12,6 +12,15 @@ impl Identifier {
     /// Creates a new [`Identifier`] after validating the input.
     ///
     /// Returns an error if the input contains invalid characters.
+    ///
+    /// ```rust
+    /// # use std::error::Error;
+    /// # fn main() -> Result<(), Box<dyn Error>> {
+    /// use zeevonk::ident::Identifier;
+    /// let id = Identifier::new("client-01")?;
+    /// assert_eq!(id.as_str(), "client-01");
+    /// # Ok(()) }
+    /// ```
     pub fn new(id: impl Into<String>) -> crate::Result<Self> {
         let id_str = id.into();
         if id_str.chars().all(|c| c.is_ascii_lowercase() || c.is_ascii_digit() || c == '-') {
@@ -24,6 +33,11 @@ impl Identifier {
     /// Returns a reference to the underlying string.
     pub fn as_str(&self) -> &str {
         &self.0
+    }
+
+    /// Consume the identifier and return the owned String.
+    pub fn into_inner(self) -> String {
+        self.0
     }
 }
 
@@ -38,5 +52,24 @@ impl std::str::FromStr for Identifier {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         Identifier::new(s)
+    }
+}
+
+impl AsRef<str> for Identifier {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+impl TryFrom<String> for Identifier {
+    type Error = crate::Error;
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        Identifier::new(s)
+    }
+}
+impl<'a> TryFrom<&'a str> for Identifier {
+    type Error = crate::Error;
+    fn try_from(s: &'a str) -> Result<Self, Self::Error> {
+        Identifier::new(s.to_owned())
     }
 }
